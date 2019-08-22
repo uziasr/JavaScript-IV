1f37f512e15fd21bb37f2b253065243699eafa3d
Uzias Rivera <uziasrivera@gmail.com>
2019-08-22 12:54:52 -0700

    created Person class and tested it with fred

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attrs){
+        this.name = attrs.name
+        this.age = attrs.age
+        this.location = attrs.location
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}` 
+    }
+}
+const fred = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  console.log(fred.speak())
