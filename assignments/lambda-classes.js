8d35495ae09a7878575d92f50bc371cf88b79090
Uzias Rivera <uziasrivera@gmail.com>
2019-08-22 13:18:02 -0700

    Student class and tested it --working--

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,14 +25,34 @@ class Instructor extends Person{
     }
 }
 
-const fred = new Instructor({
+class Student extends Person{
+    constructor(attrs){
+        super(attrs)
+        this.previousBackground = attrs.previousBackground
+        this.className = attrs.className
+        this.favSubjects = attrs.favSubjects
+    }
+    listsSubjects(){//console.log for this method is not necessary
+        this.favSubjects.forEach(element=>console.log(element))
+    }
+    PRAssignments(subject){
+        return `${this.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject){
+        return `${this.name} has begun a sprint challenge on ${subject}`
+    }
+}
+const fred = new Student({
     name: 'Fred',
     location: 'Bedrock',
     age: 37,
-    favLanguage: 'JavaScript',
-    specialty: 'Front-end',
-    catchPhrase: `Don't forget the homies`
+    favSubjects: ['JavaScript','CSS','Python','HTML'],
+    className: 'CS304',
+    previousBackground: `I graduated from college with a degree in hospitality admin`
   });
 
-console.log(fred.demo('python'))
+fred.listsSubjects()
+console.log(fred.PRAssignments('css'))
+console.log(fred.sprintChallenge('python'))
+
   
